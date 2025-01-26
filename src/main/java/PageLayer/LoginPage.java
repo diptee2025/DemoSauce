91c4cc1ba71cce057644d274046f887329cbca53
kunal <kunalgajare5@gmail.com>
2025-01-26 17:52:36 +0530

    Login Page Created by Kunal

--- src/main/java/PageLayer/LoginPage.java
+++ src/main/java/PageLayer/LoginPage.java
@@ -1,5 +1,37 @@
 package PageLayer;
 
-public class LoginPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import BaseLayer.BaseClass;
+
+public class LoginPage extends BaseClass {
+	
+	@FindBy(name = "user-name")
+	private WebElement username;
+	
+	@FindBy(name = "password")
+	private WebElement password;
+	
+	@FindBy(name = "login-button")
+	private WebElement loginButton;
+	
+	public LoginPage()
+	{
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void usernameAndPasswordFunctionality(String Username,String Password)
+	{
+		username.sendKeys(Username);
+		
+		password.sendKeys(Password);
+	}
+	
+	public void loginButtonFunctionality()
+	{
+		loginButton.click();
+	}
 
 }
--- src/test/java/StepDefinition/LoginPageStep.java
+++ src/test/java/StepDefinition/LoginPageStep.java
@@ -1,5 +1,28 @@
 package StepDefinition;
 
+import BaseLayer.BaseClass;
+import PageLayer.LoginPage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class LoginPageStep {
+	private static LoginPage loginpage;
+	
+	@Given("user is on login page")
+	public void user_is_on_login_page() {
+	   BaseClass.initialization();
+	}
+
+	@When("user enter username as {string} and password as {string}")
+	public void user_enter_username_as_and_password_as(String Username, String Password) {
+	   loginpage = new LoginPage();
+	   loginpage.usernameAndPasswordFunctionality(Username, Password);
+	}
+
+	@Then("user click on login button")
+	public void user_click_on_login_button() {
+	   loginpage.loginButtonFunctionality();
+	}
 
 }
